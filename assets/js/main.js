6ae74e8061c0ddac6bf7fa183dfe3772787cce54
aguslu0 <artifexted@gmail.com>
2022-07-12 03:00:28 -0300

    v2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 // Contenido de la playlist
 const aSongs = [
-  { title: "A la sombra", author: "Barbara Bluedeep", url: "../assets/song/a_la_sombra.mp3" },
+  { title: "Sympatico", author: "Stephen Kelly", url: "../assets/song/sympatico.mp3" },
   { title: "Cazador", author: "Germán Lucero", url: "../assets/song/cazador.mp3" },
-  { title: "Confundido", author: "Sianestesia", url: "../assets/song/confundido.mp3" },
   { title: "Corriendo desde atras", author: "Tomas Hernandez", url: "../assets/song/corriendo_desde_atras.mp3" },
   { title: "Darse cuenta", author: "The Black Ladies", url: "../assets/song/darse_cuenta.mp3" },
   { title: "El viento en la cara", author: "Kenio", url: "../assets/song/el_viento_en_la_cara.mp3" },
+  { title: "Toc toc ja ja", author: "Germán Lucero", url: "../assets/song/toc_toc_ja_ja.mp3" },
+  { title: "A la sombra", author: "Barbara Bluedeep", url: "../assets/song/a_la_sombra.mp3" },
+  { title: "Confundido", author: "Sianestesia", url: "../assets/song/confundido.mp3" },
   { title: "En mi mambo", author: "Franco Parisi", url: "../assets/song/en_mi_mambo.mp3" },
   { title: "Estás acá", author: "Rodrigo", url: "../assets/song/estas_aca.mp3" },
   { title: "Fanter", author: "Lil Grow", url: "../assets/song/fanter.mp3" },
@@ -15,8 +17,6 @@ const aSongs = [
   { title: "Paraguita", author: "ESCUDERO, Jose Nicolas", url: "../assets/song/paraguita.mp3" },
   { title: "Robar tu soledad", author: "Algo Natural", url: "../assets/song/robar_tu_soledad.mp3" },
   { title: "Si tu te vas", author: "Elias", url: "../assets/song/si_tu_te_vas.mp3" },
-  { title: "Sympatico", author: "Stephen Kelly", url: "../assets/song/sympatico.mp3" },
-  { title: "Toc toc ja ja", author: "Germán Lucero", url: "../assets/song/toc_toc_ja_ja.mp3" },
   { title: "Tu canción", author: "Rodrigo Villegas", url: "../assets/song/tu_cancion.mp3" },
   { title: "Ya no está", author: "Hijos de Apolo", url: "../assets/song/ya_no_esta.mp3" }
 ];
@@ -81,10 +81,10 @@ init();
 
 // Contenido de la playlist de BEATS
 const aBeats = [
-  { title: "Escape", url: "../assets/song/beats/escape.mp3" },
-  { title: "Loop para app", url: "../assets/song/beats/loop_app.mp3" },
-  { title: "Rabioso", url: "../assets/song/beats/rabioso.mp3" },
-  { title: "Verte", url: "../assets/song/beats/verte.mp3" }
+  { title: "Rabioso - Surfónico", url: "../assets/song/beats/rabioso.mp3" },
+  { title: "Escape - Surfónico", url: "../assets/song/beats/escape.mp3" },
+  { title: "Verte - Surfónico", url: "../assets/song/beats/verte.mp3" },
+  { title: "Loop para app - Surfónico", url: "../assets/song/beats/loop_app.mp3" }
 ];
 
 let htmlCodeBeats = `
